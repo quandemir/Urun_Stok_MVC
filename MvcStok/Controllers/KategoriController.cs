9f964a870d728c59af552285058a210f2af3bbcf
Osman KANDEMIR <kandemirosman6925@gmail.com>
2023-03-01 22:19:53 +0300

    Add project files.

---
+++ MvcStok/Controllers/KategoriController.cs
@@ -0,0 +1,60 @@
+﻿using MvcStok.Models.Entity;
+using PagedList;
+using PagedList.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace MvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        MvcDbStokEntities db=new MvcDbStokEntities();
+        public ActionResult Index(int sayfa=1)
+        {
+            //var degerler=db.TBLKATEGORILER.ToList();
+            var degerler = db.TBLKATEGORILER.ToList().ToPagedList(sayfa,4);
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniKategori()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniKategori(TBLKATEGORILER p1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("YeniKategori");    
+            }
+            db.TBLKATEGORILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult SIl(int id)
+        {
+            var deger=db.TBLKATEGORILER.Find(id);
+            db.TBLKATEGORILER.Remove(deger);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var deger = db.TBLKATEGORILER.Find(id);
+            return View("KategoriGetir", deger);
+        }
+        public ActionResult Guncelle(TBLKATEGORILER p1)
+        {
+            var ktg = db.TBLKATEGORILER.Find(p1.KATEGORIID);
+            ktg.KATEGORIAD = p1.KATEGORIAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ MvcStok/Controllers/MusteriController.cs
@@ -0,0 +1,62 @@
+﻿using MvcStok.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcStok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        MvcDbStokEntities db=new MvcDbStokEntities();
+        public ActionResult MusteriListesi(string p)
+        {
+            var degerler = from d in db.TBLMUSTERILER select d;
+            if (!string.IsNullOrEmpty(p))
+            {
+                degerler = degerler.Where(m => m.MUSTERIAD.Contains(p));
+            }
+            return View(degerler.ToList());
+            //var degerler = db.TBLMUSTERILER.ToList();
+            //return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniMusteri()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniMusteri(TBLMUSTERILER p1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("YeniMusteri");
+            }
+            db.TBLMUSTERILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult SIl(int id)
+        {
+            var deger = db.TBLMUSTERILER.Find(id);
+            db.TBLMUSTERILER.Remove(deger);
+            db.SaveChanges();
+            return RedirectToAction("MusteriListesi");
+        }
+        public ActionResult MusteriGetir(int id)
+        {
+            var deger = db.TBLMUSTERILER.Find(id);
+            return View("MusteriGetir",deger);
+        }
+        public ActionResult Guncelle(TBLMUSTERILER p1)
+        {
+            var mus = db.TBLMUSTERILER.Find(p1.MUSTERIID);
+            mus.MUSTERIAD=p1.MUSTERIAD;
+            mus.MUSTERISOYAD=p1.MUSTERISOYAD;
+            db.SaveChanges();
+            return RedirectToAction("MusteriListesi");
+        }
+    }
+}
